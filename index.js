b475d86a672f4f69f35acce9312604dd9e891e42
ayush <ayushanand754@gmail.com>
2026-07-14 23:46:07 +0530

    Newjsonres

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
-
-console.log("Hello, World!");
-console.log("This is a sample Node.js application.");
-
-
 const express = require('express');
 const app = express();
 const port = 3000;
+const ayush = {
+  "name": "Jane Doe",
+  "age": 28,
+  "is_employee": true,
+  "skills": ["Python", "JSON", "Data Analysis"],
+  "address": {
+    "city": "Patna",
+    "country": "India"
+  },
+  "manager": null
+};
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -22,7 +28,9 @@ app.get('/login', (req, res) => {
 app.get('/youtube', (req, res) => {
     res.send('<h1>ayush youtube</h1>');
 })
-
+app.get('/github', (req, res) => {
+    res.json(ayush);
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
 });
